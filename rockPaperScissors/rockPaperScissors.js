192ffa489407d5325819a1f78cbadcd9644c2552
Adrian B <adrianny90@wp.pl>
2025-03-05 10:56:21 +0100

    add misspell block

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -4,44 +4,56 @@ const bag = ["rock", "scissors", "paper"];
 const randomIndex = Math.floor(Math.random() * bag.length);
 const computer = bag[randomIndex];
 //console.log(computer);
-if (player === computer) {
-  console.log(
-    `You chose ${player} computer chose ${computer}. There is a draw.`
-  );
-} else {
-  if (player === "rock") {
-    switch (computer) {
-      case "paper":
-        console.log(
-          `You chose ${player} computer chose ${computer}. You lose...`
-        );
-        break;
-      case "scissors":
-        console.log(`You chose ${player} computer chose ${computer}. You win!`);
-        break;
-    }
-  } else if (player === "paper") {
-    switch (computer) {
-      case "scissors":
-        console.log(
-          `You chose ${player} computer chose ${computer}. You lose...`
-        );
-        break;
-      case "rock":
-        console.log(`You chose ${player} computer chose ${computer}. You win!`);
-        break;
-    }
+if (bag.includes(player)) {
+  if (player === computer) {
+    console.log(
+      `You chose ${player} computer chose ${computer}. There is a draw.`
+    );
   } else {
-    //player scissors
-    switch (computer) {
-      case "rock":
-        console.log(
-          `You chose ${player} computer chose ${computer}. You lose...`
-        );
-        break;
-      case "paper":
-        console.log(`You chose ${player} computer chose ${computer}. You win!`);
-        break;
+    if (player === "rock") {
+      switch (computer) {
+        case "paper":
+          console.log(
+            `You chose ${player} computer chose ${computer}. You lose...`
+          );
+          break;
+        case "scissors":
+          console.log(
+            `You chose ${player} computer chose ${computer}. You win!`
+          );
+          break;
+      }
+    } else if (player === "paper") {
+      switch (computer) {
+        case "scissors":
+          console.log(
+            `You chose ${player} computer chose ${computer}. You lose...`
+          );
+          break;
+        case "rock":
+          console.log(
+            `You chose ${player} computer chose ${computer}. You win!`
+          );
+          break;
+      }
+    } else {
+      //player scissors
+      switch (computer) {
+        case "rock":
+          console.log(
+            `You chose ${player} computer chose ${computer}. You lose...`
+          );
+          break;
+        case "paper":
+          console.log(
+            `You chose ${player} computer chose ${computer}. You win!`
+          );
+          break;
+      }
     }
   }
+} else {
+  console.log(
+    "You can only choose: rock, scissors or paper.\n Please do not misspell it."
+  );
 }
